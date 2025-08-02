96d2ef1d5c3d9960285211eaef908c29df800baa
Nowshin Sayara <83832789+NSR123456@users.noreply.github.com>
2025-08-02 21:41:00 +0600

    Create Valid Anagram.cs

---
+++ Valid Anagram.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        int[] cntS = new int[30];
+        int[] cntT = new int[30];
+
+        foreach(char c in s){
+            cntS[c - 'a']++;
+        }
+        foreach(char c in t){
+            cntT[c - 'a']++;
+        }
+        for(int i= 0; i<25; i++){
+            if(cntS[i] != cntT[i]) return false;
+        }
+        return true;
+    }
+}
